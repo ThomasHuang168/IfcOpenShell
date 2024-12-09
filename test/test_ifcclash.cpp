d15bd102375fb7bdde65e9949ea74c9135abb1eb
Hing HUANG <hi.huang@bouygues-construction.com>
2024-12-09 17:03:33 +0000

    working test

--- test/test_ifcclash.cpp
+++ test/test_ifcclash.cpp
@@ -17,106 +17,17 @@
  *                                                                              *
  ********************************************************************************/
 
-/********************************************************************************
- *                                                                              *
- * This started as a brief example of how IfcOpenShell can be interfaced from   *
- * within a C++ context, it has since then evolved into a fullfledged command   *
- * line application that is able to convert geometry in an IFC files into       *
- * several tessellated and topological output formats.                          *
- *                                                                              *
- ********************************************************************************/
-
-// windows stuff: defines max as a macro when including windows.h
-// error C2589: '(': illegal token on right side of '::'
-#define NOMINMAX
-
-// TODO: Multiple schemas
-#define IfcSchema Ifc2x3
-
-#include "../src/ifcparse/IfcFile.h"
-#include "../src/ifcparse/IfcLogger.h"
-#include "../src/ifcparse/Ifc2x3.h"
-
-#include <boost/preprocessor/stringize.hpp>
-#include <boost/preprocessor/seq/for_each.hpp>
-#include <boost/preprocessor/seq/size.hpp>
-#include <boost/preprocessor/seq/pop_back.hpp>
-#include <boost/preprocessor/comparison/greater.hpp> 
-#include <boost/preprocessor/selection/min.hpp>
-
-#include "../src/serializers/ColladaSerializer.h"
-#include "../src/serializers/GltfSerializer.h"
-#include "../src/serializers/HdfSerializer.h"
-#include "../src/serializers/IgesSerializer.h"
-#include "../src/serializers/StepSerializer.h"
-#include "../src/serializers/WavefrontObjSerializer.h"
-#include "../src/serializers/XmlSerializer.h"
-#include "../src/serializers/SvgSerializer.h"
-#include "../src/serializers/USDSerializer.h"
-#include "../src/serializers/TtlWktSerializer.h"
-            
-#include "../src/ifcgeom/IfcGeomFilter.h"
-#include "../src/ifcgeom/Iterator.h"
-#include "../src/ifcgeom/IfcGeomRenderStyles.h"
-
-#include "../src/ifcgeom/taxonomy.h"
-#include "../src/ifcgeom/piecewise_function_evaluator.h"
-#ifdef IFOPSH_WITH_OPENCASCADE
-    #include "../src/ifcgeom/Serialization/Serialization.h"
-    #include "../src/ifcgeom/kernels/opencascade/IfcGeomTree.h"
-
-    #include <BRepTools_ShapeSet.hxx>
-#endif
-
-#include "../src/ifcparse/utils.h"
+ /********************************************************************************
+  *                                                                              *
+  * This started as a brief example of how IfcOpenShell can be interfaced from   *
+  * within a C++ context, it has since then evolved into a fullfledged command   *
+  * line application that is able to convert geometry in an IFC files into       *
+  * several tessellated and topological output formats.                          *
+  *                                                                              *
+  ********************************************************************************/
 
 #ifdef IFOPSH_WITH_OPENCASCADE
-
-#include <Standard_Version.hxx>
-
-#if OCC_VERSION_HEX < 0x60900
-#include <IGESControl_Controller.hxx>
-#endif
-
-#endif
-
-#include <boost/program_options.hpp>
-#include <boost/make_shared.hpp>
-
-#include <fstream>
-#include <sstream>
-#include <set>
-#include <time.h>
-#include <iomanip>
-
-#if USE_VLD
-#include <vld.h>
-#endif
-
-#ifdef _MSC_VER
-#include <io.h>
-#include <fcntl.h>
-#endif
-
-#include <random>
-#include <thread>
-
-#if defined(_MSC_VER) && defined(_UNICODE)
-typedef std::wstring path_t;
-typedef std::wofstream ofstream_t;
-static std::wostream& cout_ = std::wcout;
-static std::wostream& cerr_ = std::wcerr;
-#else
-typedef std::string path_t;
-typedef std::ofstream ofstream_t;
-static std::ostream& cout_ = std::cout;
-static std::ostream& cerr_ = std::cerr;
-#endif
-
-#include "../src/ifcgeom/IfcGeomFilter.h"
-#include "../src/ifcgeom/IfcGeomRenderStyles.h"
-#include "../src/ifcgeom/Iterator.h"
-#include "../src/ifcparse/utils.h"
+#include "../src/ifcgeom/kernels/opencascade/IfcGeomTree.h"
 
 #include <gtest/gtest.h>
 
@@ -124,71 +35,37 @@ TEST(Boolean, readIfcGeom) {
     bool check_all = true;
     double tolerance = 0.000001;
 
+    IfcGeom::tree tree;
+    auto add_file = [&tree](IfcParse::IfcFile& f) {
+        IfcGeom::Iterator it("opencascade", ifcopenshell::geometry::Settings{}, &f, {}, 1);
+        if (it.initialize()) {
+            do {
+                tree.add_element(dynamic_cast<IfcGeom::TriangulationElement*>(it.get()));
+            } while (it.next());
+        }
+    };
 
-    IfcGeom::tree* _tree;
+    IfcParse::IfcFile file_a("C:\\Users\\HI.HUANG\\Downloads\\A.ifc");
+    add_file(file_a);
+
+    IfcParse::IfcFile file_b("C:\\Users\\HI.HUANG\\Downloads\\B.ifc");
+    add_file(file_b);
+
     std::vector<const IfcUtil::IfcBaseEntity*> set_a_entities;
     std::vector<const IfcUtil::IfcBaseEntity*> set_b_entities;
-    IfcParse::IfcFile* file_a = new IfcParse::IfcFile("C:/Users/HI.HUANG/Downloads/A.ifc");
-    _tree = new IfcGeom::tree(*file_a);
-    //_tree = new IfcGeom::tree();
-    //
-    //ifcopenshell::geometry::Settings settings_ = ifcopenshell::geometry::Settings{};
-    //settings_.get<ifcopenshell::geometry::settings::IteratorOutput>().value = //ifcopenshell::geometry::settings::NATIVE;
-    //settings_.get<ifcopenshell::geometry::settings::UseWorldCoords>().value = true;
-    //settings_.get<ifcopenshell::geometry::settings::ReorientShells>().value = true;
-    //
-    //IfcGeom::Iterator it(settings_, file_a, {}, 1);
-    //if (it.initialize()) {
-    //    do {
-    //        auto* elem = it.get();
-    //        if (elem == NULL) {
-    //            continue;
-    //        }
-    //        _tree->add_element(dynamic_cast<IfcGeom::TriangulationElement*>(elem));
-    //    } while (it.next());
-    //}
 
+    //auto walls = file_a.instances_by_type("IfcReinforcingbar");
+    //auto slabs = file_b.instances_by_type("IfcBuildingElementProxy");
+    auto walls = file_a.instances_by_type("IfcProduct");
+    auto slabs = file_b.instances_by_type("IfcProduct");
+    std::transform(walls->begin(), walls->end(), std::back_inserter(set_a_entities), [](auto* x) { return x->as<IfcUtil::IfcBaseEntity>(); });
+    std::transform(slabs->begin(), slabs->end(), std::back_inserter(set_b_entities), [](auto* x) { return x->as<IfcUtil::IfcBaseEntity>(); });
 
-    IfcSchema::IfcProduct::list::ptr elements = (*file_a).instances_by_type<IfcSchema::IfcProduct>();
+    std::vector<IfcGeom::clash> clash_result = tree.clash_intersection_many(set_a_entities, set_b_entities, tolerance, check_all);
 
-    std::cout << "Found " << elements->size() << " elements" << std::endl;
-
-    for (auto it_a = elements->begin(); it_a != elements->end(); ++it_a) {
-        auto* element = *it_a;
-        element->toString(std::cout);
-        std::cout << std::endl;
-        if (!element->declaration().is("IfcProduct")) {
-            throw IfcParse::IfcException("All instances should be of type IfcProduct");
-        }
-        set_a_entities.push_back(static_cast<IfcUtil::IfcBaseEntity*>(element));
-    }
-    IfcParse::IfcFile* file_b = new IfcParse::IfcFile("C:/Users/HI.HUANG/Downloads/B.ifc");
-    _tree->add_file(*file_b, ifcopenshell::geometry::Settings{});
-    //IfcGeom::Iterator it_b(settings_, file_b, {}, 1);
-    //if (it_b.initialize()) {
-    //    do {
-    //        auto* elem = it_b.get();
-    //        if (elem == NULL) {
-    //            continue;
-    //        }
-    //        _tree->add_element(dynamic_cast<IfcGeom::TriangulationElement*>(elem));
-    //    } while (it_b.next());
-    //}
-
-    elements = (*file_b).instances_by_type<IfcSchema::IfcProduct>();
-
-    std::cout << "Found " << elements->size() << " elements" << std::endl;
-
-    for (auto it_b_b = elements->begin(); it_b_b != elements->end(); ++it_b_b) {
-        auto* element = *it_b_b;
-        element->toString(std::cout);
-        std::cout << std::endl;
-        if (!element->declaration().is("IfcProduct")) {
-            throw IfcParse::IfcException("All instances should be of type IfcProduct");
-        }
-        set_b_entities.push_back(static_cast<IfcUtil::IfcBaseEntity*>(element));
-    }
-    std::vector<IfcGeom::clash> clash_result = _tree->clash_intersection_many(set_a_entities, set_b_entities, tolerance, check_all);
+    std::cout << "n clashes " << clash_result.size() << std::endl;
 
     ASSERT_TRUE(1);
 }
+
+#endif
